fabc50e28c2ca9a0987a0ba14b5cefcc7904767d
Andrew <anedward96@gmail.com>
2019-06-05 13:45:56 -0500

    Agregacion de atributos de detalleFac

--- src/main/java/ec/edu/ups/entidades/DetalleFacturaEN.java
+++ src/main/java/ec/edu/ups/entidades/DetalleFacturaEN.java
@@ -4,5 +4,46 @@ package ec.edu.ups.entidades;
 
 public class DetalleFacturaEN {
 		
+	private int codigo;
+	private int cantidad;
+	private double precioUnitario;
+	private double precioTotal;
+	
+	public DetalleFacturaEN() {
+
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	
 	
 }
